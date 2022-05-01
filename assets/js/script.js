74b26c901ec6a48b1a01c404665c0fa670aa4293
sakau <sakau@usc.edu>
2022-05-01 15:52:27 -0700

    finished search and header; fixed enter button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,26 @@ var formSubmitHandler = function(event) {
 
     if (location) {
         searchedLocation.value = "";
-        getLocation(location)
+        getParkInfo(location);
     } else {
         // TODO: Handle empty search bar
     }
 }
 
-// Turns City into Lat Lon
-var getLocation = function (location) {
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&appid=e1fdfa2386872dd651201114b0cdeacd";
-    fetch(apiUrl).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                var lat = data[0].lat
-                var lon = data[0].lon
-                getParkInfo(lat, lon, location);
-            })
-        } else {
-            alert("An Error Occured");
-        }
-    })
-        .catch(function (error) {
-            alert("Unable to Connect to Open Weather Map");
-        })
+// Runs when enter button is clicked
+var formSubmitHandlerEnter = function() {
+    var location = searchedLocation.value.trim();
+
+    if (location) {
+        searchedLocation.value = "";
+        getParkInfo(location);
+    } else {
+        // TODO: Handle empty search bar
+    }
 }
 
 // Links the website to the park API
-var getParkInfo = function(lat, lon, location) {
+var getParkInfo = function(location) {
     console.log(location);
     var apiUrl = "https://developer.nps.gov/api/v1/parks?stateCode=" + location + "&api_key=mAgL5ygwIf8s4dQRtaUvaEjd3ZhfFsCBQATeElnc"    
     fetch(apiUrl).then(function(response){
@@ -58,3 +51,11 @@ var getParkInfo = function(lat, lon, location) {
 
 // Event listener for the submit button
 searchBtn.addEventListener("click", formSubmitHandler);
+// Event listener for 'enter' key
+searchedLocation.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        formSubmitHandlerEnter();
+       
+    }
+  });
