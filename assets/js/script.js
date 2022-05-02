546b3a25ab59417d37ac58fb61d647ddb3140dbf
sakau <sakau@usc.edu>
2022-05-02 13:54:27 -0700

    finished feature. added function to display park information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Variables
 var searchBtn = document.querySelector("#search-btn");
 var searchedLocation = document.querySelector("#searched-location")
+var parkContainer = document.querySelector("#park-container");
 
 // Runs when submit button is clicked
 var formSubmitHandler = function(event) {
@@ -35,7 +36,7 @@ var getParkInfo = function(location) {
     fetch(apiUrl).then(function(response){
         if(response.ok) {
             response.json().then(function(data){
-                console.log(data);
+                displayParks(data);
             })
         } else {
             alert("An Error Occured");
@@ -44,6 +45,38 @@ var getParkInfo = function(location) {
     .catch(function(error){
         alert("Unable to connect to Natioal Park Services")
     })
+}
+
+// Function Displays park Info
+var displayParks = function(location) {
+    console.log(location);
+
+    for (i = 0; i < 5; i++) {
+    var parkCard = document.createElement("a")
+    parkCard.classList = "list-group-item list-group-item-action flex-column align-items-start"
+    var subParkCard = document.createElement("div")
+    subParkCard.classList = "d-flex w-100 justify-content-between"
+    
+    var parkName = document.createElement("h5")
+    parkName.className = "mb-1"
+    parkName.textContent = location.data[i].fullName
+    var parkContact = document.createElement("small")
+    parkContact.textContent = "Phone: " + location.data[i].contacts.phoneNumbers[0].phoneNumber
+
+    subParkCard.appendChild(parkName)
+    subParkCard.appendChild(parkContact)
+    
+    var parkDesc = document.createElement("p")
+    parkDesc.className = "mb-1"
+    parkDesc.textContent = location.data[i].description
+    var parkAddress = document.createElement("small")
+    parkAddress.textContent = location.data[i].addresses[0].city + ", " + location.data[i].addresses[0].stateCode + " " + location.data[i].addresses[0].postalCode; 
+
+    parkCard.appendChild(subParkCard);
+    parkCard.appendChild(parkDesc);
+    parkCard.appendChild(parkAddress);
+    parkContainer.appendChild(parkCard);
+    }
 }
   
 // Event listener for the submit button
