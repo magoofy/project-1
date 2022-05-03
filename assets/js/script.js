5e4ad228034138472b5f0e7263a56ec8e659e49a
Jolo <jolocabrera@yahoo.com>
2022-05-02 18:44:42 -0700

    base layer map

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+  // var coordinate = [-119,37]
+  var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([-119, 37]),
+      zoom: 6
+    })
+  });
